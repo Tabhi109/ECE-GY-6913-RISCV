a688859aa7d371b8b5404fbb7f46d2138250fdf6
wangyihao <yihao.w@nyu.edu>
2023-10-24 16:32:32 -0400

    some refactor

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ECE-GY-6913-RISCV
+
+[project page](https://against-entropy.notion.site/RISC-V-simulator-3364c353830c4286928c2343b99e3a6f)
+
---
+++ constants.py
@@ -0,0 +1,6 @@
+MemSize = 1000 
+IMEM_FILE = "imem.txt"
+DMEM_FILE = "dmem.txt"
+RF_FILE = "RFresult.txt"
+SS_STATERESULT_FILE = "StateResult_SS.txt"
+FS_STATERESULT_FILE = "StateResult_FS.txt"
