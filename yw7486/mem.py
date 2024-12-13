5d390df19f8a8ee795b6022e26040fc1cb84ec2a
Abhinav Tripathi <93431100+Tabhi109@users.noreply.github.com>
2024-12-14 04:06:00 +0530

    Update mem.py

--- yw7486/mem.py
+++ yw7486/mem.py
@@ -5,43 +5,42 @@ from constants import (BYTE_LEN, DMEM_FILE, DMEM_RESULT_FILE, IMEM_FILE,
 from misc import signed_binary_str_to_int, signed_int_to_binary_str
 
 
-class InsMem(object):
-    def __init__(self, name: str, ioDir: Path):
-        self.id = name
+class InstructionMemory(object):
+    def __init__(self, identifier: str, io_directory: Path):
+        self.identifier = identifier
 
-        with open(ioDir / IMEM_FILE) as im:
-            self.IMem = [data.replace("\n", "") for data in im.readlines()]
+        with open(io_directory / IMEM_FILE) as instruction_file:
+            self.memory = [line.replace("\n", "") for line in instruction_file.readlines()]
 
-    def readInstr(self, ReadAddress: int):
-        # read instruction memory
-        # return 32 bit hex val
-        return "".join(self.IMem[ReadAddress: ReadAddress + WORD_LEN])  # read in 4 lines and cocnat them into a 32-bit binary string
+    def fetchInstruction(self, address: int):
+        # Fetch instruction from memory
+        # Return 32-bit hex value
+        return "".join(self.memory[address: address + WORD_LEN])  # Read 4 lines and concatenate them into a 32-bit binary string
 
 
-class DataMem(object):
-    def __init__(self, name: str, ioDir: Path, outDir: Path):
-        self.id = name
-        self.ioDir = ioDir
-        self.outDir = outDir
-        with open(ioDir / DMEM_FILE) as dm:
-            self.DMem = [data.replace("\n", "") for data in dm.readlines()]
+class DataMemory(object):
+    def __init__(self, identifier: str, io_directory: Path, output_directory: Path):
+        self.identifier = identifier
+        self.io_directory = io_directory
+        self.output_directory = output_directory
+        with open(io_directory / DMEM_FILE) as data_file:
+            self.memory = [line.replace("\n", "") for line in data_file.readlines()]
         
-        # fill the rest of the memory with 0
-        self.DMem.extend(['00000000'] * (MemSize - len(self.DMem)))
+        # Fill the rest of the memory with zeros
+        self.memory.extend(['00000000'] * (MemSize - len(self.memory)))
 
-    def readDataMem(self, ReadAddress: int):
-        # read data memory
-        binary_str = "".join(self.DMem[ReadAddress: ReadAddress + WORD_LEN])
+    def readData(self, address: int):
+        # Read data from memory
+        binary_str = "".join(self.memory[address: address + WORD_LEN])
         return signed_binary_str_to_int(binary_str)
 
-    def writeDataMem(self, Address: int, WriteData: int):
-        # write data into byte addressable memory
-        binary_str = signed_int_to_binary_str(WriteData)
+    def writeData(self, address: int, data: int):
+        # Write data into byte-addressable memory
+        binary_str = signed_int_to_binary_str(data)
         for i in range(WORD_LEN):
-            self.DMem[Address + i] = binary_str[BYTE_LEN*i: BYTE_LEN*(i+1)]
+            self.memory[address + i] = binary_str[BYTE_LEN*i: BYTE_LEN*(i+1)]
 
-    def outputDataMem(self):
-        resPath = self.outDir / f"{self.id}_{DMEM_RESULT_FILE}"
-        with open(resPath, "w") as rp:
-            rp.writelines([str(data) + "\n" for data in self.DMem])
-
+    def exportDataMemory(self):
+        result_path = self.output_directory / f"{self.identifier}_{DMEM_RESULT_FILE}"
+        with open(result_path, "w") as result_file:
+            result_file.writelines([str(data) + "\n" for data in self.memory])
